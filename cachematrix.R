3d774fdca85b52c590edb085349e26092e19fa69
karltoolbox <karlsigfrid@gmail.com>
2015-09-21 01:13:01 +0200

    Updating the file according to the assignment.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+##The function defines 4 nested functions,
+## which are returned as elements of a list.
+##(Only setinverse is actually used)
+##Setinverse puts the inverse matrix in a global object.
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(my_matrix = matrix()) {
+		set <- function(y) {
+               my_matrix <<- y
+                my_inverse <<- NULL
+        }
+        get <- function() my_matrix
+        setinverse <- function(this_inverse){
+		 assign("my_inverse", this_inverse, envir = .GlobalEnv) 
+		}
+        getinverse <- function() my_inverse
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+##This function takes a matrix as its first argument.
+##It reads the list of functions into ks.
+##It checks whether the global variable my_inverse has been created.
+##If true, the object is returned.
+##If empty, the inverse is calculated and stored in the global matrix my_inverse.
+##The function returns the inverted matrix.
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(my_matrix, ...) {
         ## Return a matrix that is the inverse of 'x'
+        ks<-makeCacheMatrix()
+	if (exists("my_inverse")){
+                print("getting cached data")  
+                return(my_inverse)
+        }
+        this_inverse <- solve(my_matrix, ...)
+        ks$setinverse(this_inverse)
+        my_inverse
+
+
 }
